a4f747e20ff1f3d71fb2eaf7a28cbdbd73dfd05d
seung-ik <69374857+seung-ik@users.noreply.github.com>
2023-05-08 22:10:00 +0900

    feat: add ga code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,19 @@ import store from '@store/index';
 import RootLayout from '@layout/RootLayout';
 import GlobalStyle from '@styles/GlobalStyle';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+
 const UserStatus = dynamic(() => import('../components/articles/UserStatus'), {
   ssr: false,
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -21,6 +26,22 @@ export default function App({ Component, pageProps }: AppProps) {
       },
     },
   });
+
+  useEffect(() => {
+    const code = process.env.NEXT_PUBLIC_GA_ID as string; 
+    ReactGA.initialize(code); // 추적 ID를 여기에 입력합니다.
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
+    // 페이지 이동 시에도 Google Analytics에 페이지를 추적하도록 설정합니다.
+    const handleRouteChange = (url: any) => {
+      ReactGA.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
 
   return (
     <>
